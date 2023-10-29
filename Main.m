5485f4c6adbef1ebc39f7421453abd7cf3ff80fe
Robbie <robert.howe@student.uts.edu.au>
2023-10-29 20:45:23 +1100

    added rot and trans and calib bag

--- Main.m
+++ Main.m
@@ -12,12 +12,23 @@ depthTopicMessageNum = depthTopic.NumMessages;
 depthImagesOut = readMessages(depthTopic);
 
 
+calibBag = rosbag('CalibNew_360.bag');
+calibTopic = select(calibBag, 'Topic', '/ndi_aurora');
+calibTopicMessageNum = calibTopic.NumMessages;
+calibMessages = readMessages(calibTopic, 'DataFormat', 'struct');
+
+
 %% Depth camera intrinsics extracted from ModelNewBag topic /camera/depth/camera_info
 K = [421.7674560546875, 0, 423.2069396972656, 0, 421.7674560546875, 239.1299591064453, 0, 0, 1];
 focalLength = K(1);
 principalPoint = [K(3), K(6)]; %x and y respectively
 depthScaleFactor = 5e3;
 
+%% Define the indices of the selected point clouds
+selectedIndices = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19, 22, 23, 24, 26, 27, 28, 29, 30, 79, 80, 81, 82, 88, 89, 90, 93, 104, 106, 107, 108, 127, 131, 134, 135, 136, 137, 138, 140, 150, 152, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 190, 191];
+
+% Initialize an empty cell array to store the selected point clouds
+selectedPointClouds = cell(1, numel(selectedIndices));
 
 %% Iterate through depth images 51-100 and create a point cloud
 pointClouds = cell(1, depthTopicMessageNum);
@@ -50,6 +61,80 @@ for k = [1, 50, 117, 118, 196, 201]
     zlabel('Z (m)');
 end
 
+
+
+%% BElow is commit for the rot and transl does not use above code which is duplicate
+%% Define the camera intrinsics for all depth images (outside the loop)
+        % intrinsicsList = cell(1, depthTopicMessageNum);
+        % for k = 1:depthTopicMessageNum
+        %     imageSize = size(depthImagesOut{k}, [1, 2]);
+        %     intrinsicsList{k} = cameraIntrinsics(focalLength, principalPoint, imageSize);
+        % end
+        % 
+        % %% Iterate through the selected point clouds and store them in the cell array
+        % for i = 1:numel(selectedIndices)
+        %     index = selectedIndices(i);
+        %     depthImage = readImage(depthImagesOut{index});
+        % 
+        %     % Calculate intrinsics for the current depth image using the correct index
+        %     imageSize = size(depthImage, [1, 2]);
+        %     intrinsics = intrinsicsList{index};
+        % 
+        %     % Convert the depth image to a 3D point cloud using the correct intrinsics
+        %     pointCloud = createPointCloud(depthImage, intrinsics, depthScaleFactor);
+        % end
+        % 
+        %     %% Point Cloud Rotation and Translation
+        %     %this is for translating and rotating point clouds 
+        %     % it works quite well can chose to eith just do maxs selected clouds or all of them 
+        %     %thing is all of them the rotation is slightly off since there
+        %     %is 546 calib messages and only 213 depth not sure
+        % 
+        %     hold on;
+        % for i = 1:50
+        % 
+        %     rotation = calibMessages{i}.PortHandlePoses.Rotation;
+        %     translation = calibMessages{i}.PortHandlePoses.Translation;
+        % 
+        % 
+        %     % Extract the quaternion components
+        %     rotationX = rotation.X;
+        %     rotationY = rotation.Y;
+        %     rotationZ = rotation.Z;
+        %     rotationW = rotation.W;
+        % 
+        %     rot = [rotationX rotationY rotationZ rotationW];
+        % 
+        %     translationX = translation.X;
+        %     translationY = translation.Y;
+        %     translationZ = translation.Z;
+        % 
+        %     rotRot = quat2rotm(rot);
+        % 
+        % 
+        %     % rotationAngles = [rotRot.X,rotRot.Y,rotRot.Z]; 
+        %     translation = [0,0,0];
+        %     tform = rigidtform3d(rotRot,translation);
+        % 
+        %     %Transform each point cloud.
+        %     ptCloudOut = pctransform(pointCloud,tform);
+        % 
+        %     % Store the point cloud in the selectedPointClouds array
+        %     selectedPointClouds{i} = ptCloudOut;
+        % 
+        %      lowResPointCloud = pcdownsample(selectedPointClouds{i}, 'nonuniformGridSample', 99);
+        %     %   figure;
+        %     % pcshow(lowResPointCloud);
+        %     % title('3D Point Cloud');
+        %     % xlabel('X (m)');
+        %     % ylabel('Y (m)');
+        %     % zlabel('Z (m)');
+        % 
+        %      pcshow(lowResPointCloud,'VerticalAxis','Y','VerticalAxisDir','Down');
+        %     hold on;
+        % end
+
+%% END my commit 
 
 
 
